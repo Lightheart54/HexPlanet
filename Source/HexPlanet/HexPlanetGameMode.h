1476e00cd249e15c6cd51e6db4641f3231f8b6fa
Lightheart <cullen.sarles@gmail.com>
2016-01-23 08:41:28 -0500

    Initial Commit

---
+++ Source/HexPlanet/HexPlanetGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "HexPlanetGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEXPLANET_API AHexPlanetGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/HexPlanet/Private/HexSphere.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HexPlanet.h"
+#include "HexSphere.h"
+
+
+// Sets default values
+AHexSphere::AHexSphere()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHexSphere::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHexSphere::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
